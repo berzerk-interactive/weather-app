1c0674d4a1622d705ee38feca31b072224f9402f
leeblazek <leeblazek@gmail.com>
2021-12-30 16:23:37 -0700

    add mui and autocomplete

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Demo from './demo';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Demo />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
