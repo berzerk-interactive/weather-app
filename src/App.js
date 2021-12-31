5dabf2d97fb95d4b1a25d6515b5314aaa0f2041e
leeblazek <leeblazek@gmail.com>
2021-12-30 17:43:28 -0700

    display forecast list

--- src/App.js
+++ src/App.js
@@ -18,10 +18,18 @@ function App() {
     getWeather(location)
   }
   function getWeather(place){
+    // weather?lat={lat}&lon={lon}&appid={apiKey}
     fetch(`${baseUrl}forecast?appid=${apiKey}&q=${place}`)
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => {
+        setWeather(data)
+        console.log(data)
+      });
   }
+
+  // let weatherList = weather?.list?.map((item, index) => {
+  //   return <li key={index}>{item.dt}</li>
+  // })
   return (
     <div className="App">
       <header className="App-header">
@@ -32,10 +40,54 @@ function App() {
             onChange={e => setLocation(e.target.value)}></input>
           <button type='submit' value="Submit">Get Weather</button>
         </form>
-
+        <ul>
+          {weather?.list?.map((item, index) => {
+            return <li key={index}>
+              <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} width="50" height="50" />
+                <span>{item.dt_txt} </span>
+                <span>{item.weather[0].description}</span>
+              </li>
+          })}
+        </ul>
       </header>
     </div>
   );
 }
 
 export default App;
+// {
+//   "dt": 1640919600,
+//     "main": {
+//     "temp": 277.77,
+//       "feels_like": 272.57,
+//         "temp_min": 276.98,
+//           "temp_max": 277.77,
+//             "pressure": 982,
+//               "sea_level": 982,
+//                 "grnd_level": 807,
+//                   "humidity": 29,
+//                     "temp_kf": 0.79
+//   },
+//   "weather": [
+//     {
+//       "id": 800,
+//       "main": "Clear",
+//       "description": "clear sky",
+//       "icon": "01n"
+//     }
+//   ],
+//     "clouds": {
+//     "all": 0
+//   },
+//   "wind": {
+//     "speed": 8.82,
+//       "deg": 254,
+//         "gust": 20.39
+//   },
+//   "visibility": 10000,
+//     "pop": 0.04,
+//       "sys": {
+//     "pod": "n"
+//   },
+//   "dt_txt": "2021-12-31 03:00:00"
+// }
