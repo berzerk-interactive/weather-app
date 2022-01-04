f68951c88a19b38dc8e986ac5846978b069c7caf
leeblazek <leeblazek@gmail.com>
2022-01-03 21:57:00 -0700

    Styling and dayjs

--- src/App.js
+++ src/App.js
@@ -5,17 +5,38 @@ import WeatherList  from './components/weatherlist/WeatherList';
 let apiKey = process.env.REACT_APP_API_KEY
 let baseUrl = process.env.REACT_APP_BASE_URL
 
+
 function App() {
   const [location, setLocation] = useState("");
   const [weather, setWeather] = useState();
+  const [error, setError] = useState()
+  useEffect(() => {
+    if(location?.length == 0){
+      setError(null)
+    }
+  }, [location])
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     getWeather(location)
   }
+  const handleReset = (evt) => {
+    evt.preventDefault();
+    setLocation(null)
+    setWeather(null)
+  }
+  function handleErrors(response) {
+    if (!response.ok) {
+      console.log(response);
+      setError(`${response.status} ${response.statusText} Please try again`)
+      throw Error(response.statusText);
+    }
+    return response;
+  }
   function getWeather(place){
     // weather?lat={lat}&lon={lon}&appid={apiKey}
     fetch(`${baseUrl}forecast?appid=${apiKey}&q=${place}`)
+      .then(handleErrors)
       .then(response => response.json())
       .then(data => {
         setWeather(data)
@@ -23,15 +44,28 @@ function App() {
       });
   }
 
+
+
   return (
     <div className="App">
-      <form onSubmit={handleSubmit} className={weather?.city?.name ? '' : 'centered'}>
-        <input type="text" placeholder='Enter location'
-          autoFocus
-          value={location}
-          onChange={e => setLocation(e.target.value)}></input>
-        <button type='submit' value="Submit">Get Weather</button>
-      </form>
+      <h1>Simple. Cool. Best Weather App. Ever.</h1>
+      {weather?.city?.name ?
+        (<form onSubmit={handleReset}>
+          <button type='submit' value="Submit" className="pure-button pure-button-warning">Get a different city</button>
+        </form>)
+        :
+        (<form onSubmit={handleSubmit} className={weather?.city?.name ? 'pure-form' : 'pure-form centered'} >
+          <input type="text" placeholder='Enter location'
+            autoFocus
+            value={location}
+            onChange={e => setLocation(e.target.value)}></input>
+          <button type='submit' value="Submit" className="pure-button pure-button-primary">Get Weather Forecast</button>
+          <div className='error'>
+            {error}
+          </div>
+        </form>)
+      }
+
       {weather?.city?.name ? <h1>Weather Forcast For: {weather?.city?.name}</h1> :''}
       <WeatherList list={weather?.list}></WeatherList>
     </div>
